373a56370f175132a303ad7ebfbf862714eb93da
Kaushal Gautam <kaushalgautam.alt@gmail.com>
2021-12-25 09:16:43 +0530

    challenge 10 boatSearch

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,19 +1,38 @@
 import { LightningElement } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
 
-export default class BoatSearch extends LightningElement {
+export default class BoatSearch extends NavigationMixin(LightningElement) {
 	isLoading = false;
 
 	// Handles loading event
-	handleLoading() {}
+	handleLoading() {
+		console.log("handleLoading");
+		this.isLoading = true;
+	}
 
 	// Handles done loading event
-	handleDoneLoading() {}
+	handleDoneLoading() {
+		console.log("handleDoneLoading");
+		this.isLoading = false;
+	}
 
 	// Handles search boat event
 	// This custom event comes from the form
 	searchBoats(event) {
 		console.dir(event.detail.boatTypeId);
+		let boatTypeId = event.detail.boatTypeId;
+		this.template.querySelector("c-boat-search-results").searchBoats(boatTypeId);
 	}
 
-	createNewBoat() {}
+	createNewBoat() {
+		console.log("Creating New Boat");
+		this[NavigationMixin.Navigate]({
+			type: "standard__objectPage",
+			attributes: {
+				objectApiName: "Boat__c",
+				actionName: "new"
+			},
+			state: {}
+		});
+	}
 }
