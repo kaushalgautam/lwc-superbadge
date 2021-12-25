7c6b943f23dad2d8bf61471555fcef1a61542bb4
Kaushal Gautam <kaushalgautam.alt@gmail.com>
2021-12-25 09:17:07 +0530

    fix

--- force-app/main/default/lwc/boatTile/boatTile.js
+++ force-app/main/default/lwc/boatTile/boatTile.js
@@ -10,13 +10,13 @@ export default class BoatTile extends LightningElement {
 
 	// Getter for dynamically setting the background image for the picture
 	get backgroundStyle() {
-		return "background-image:url(${this.boat.Picture__c})";
+		return `background-image:url(${this.boat.Picture__c})`;
 	}
 
 	// Getter for dynamically setting the tile class based on whether the
 	// current boat is selected
 	get tileClass() {
-		return this.selectedBoatId ? TILE_WRAPPER_SELECTED_CLASS : TILE_WRAPPER_UNSELECTED_CLASS;
+		return this.selectedBoatId === this.boat.Id ? TILE_WRAPPER_SELECTED_CLASS : TILE_WRAPPER_UNSELECTED_CLASS;
 	}
 
 	// Fires event with the Id of the boat that has been selected.
